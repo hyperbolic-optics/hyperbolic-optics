31fb9381639184dc790e50258cf801d7debcbcff
Mark Cunningham <mcunningham0410@gmail.com>
2022-11-18 12:46:28 +0000

    new additions - heatmaps included

--- ATR_calculation.py
+++ ATR_calculation.py
@@ -42,14 +42,14 @@ def permittivity_calc(wavenumber, high_freq, omega_Tn, gamma_Tn, omega_Ln, gamma
 
 
 def permittivity_fetch(wavenumber, params):
-    E_xx = []
-    E_zz = []
+    E_ext = []
+    E_ord = []
     for element in wavenumber:
-        E_xx.append(permittivity_calc(element, **params["extraordinary"]))
-        E_zz.append(permittivity_calc(element, **params["ordinary"]))
+        E_ext.append(permittivity_calc(element, **params["extraordinary"]))
+        E_ord.append(permittivity_calc(element, **params["ordinary"]))
     
-    E_xx, E_zz = np.asarray(E_xx), np.asarray(E_zz)
-    return E_xx, E_zz
+    E_ext, E_ord = np.asarray(E_ext), np.asarray(E_ord)
+    return E_ext, E_ord
 
 def plot_base_permittivity(wavenumber, E_xx, E_zz):
     plt.rcParams["figure.figsize"] = (5,4)
@@ -65,19 +65,19 @@ def plot_base_permittivity(wavenumber, E_xx, E_zz):
 
     plt.show()
 
-def rotated_permittivities(E_ext, E_ord, rotation = git snp.pi/4.):
+def rotated_permittivities(E_ext, E_ord, rotation = np.pi):
     E_xx = E_ext * (np.cos(rotation))**2 + E_ord * (np.sin(rotation))**2
     E_zz = E_ext * (np.sin(rotation))**2 + E_ord * (np.cos(rotation))**2
     
-    E_xz = E_ord * np.cos(rotation) * np.sin(rotation) - E_ext * np.cos(rotation) * np.sin(rotation)
-
+    E_xz = (E_ord - E_ext) * np.cos(rotation) * np.sin(rotation)
+    
     return E_xx, E_zz, E_xz
 
 
 def plot_rotated_permittivities(wavenumber, E_xx, E_zz, E_xz):
     plt.rcParams["figure.figsize"] = (7,5)
     fig, axs = plt.subplots(3)
-    fig.suptitle('Permittivities')
+    fig.suptitle('Permittivities at 90 Degrees')
     axs[0].plot(wavenumber, E_xx.real)
     axs[0].plot(wavenumber, E_xx.imag)
     axs[0].set(xlabel='Wavenumber', ylabel = 'E_xx')
@@ -92,13 +92,13 @@ def plot_rotated_permittivities(wavenumber, E_xx, E_zz, E_xz):
 
     plt.show()
 
-def kvectors(wavenumber, E_xx, E_zz, E_xz, prism_permittivity = 5.5, angle=np.pi/4., rotation = np.pi/4.):
+def kvectors(wavenumber, E_xx, E_zz, E_xz, prism_permittivity = 5.5, angle=np.pi/4.):
     k0 = wavenumber * 2 * np.pi
     kx = k0 * np.sqrt(prism_permittivity) * np.sin(angle)
 
     a = - E_zz
     b = - 2. * E_xz * kx
-    c = - E_xx * kx**2 - k0**2 * (E_xx * E_zz - E_xz**2)
+    c = - E_xx * kx**2 + k0**2 * (E_xx * E_zz - E_xz**2)
 
     kz1 = np.sqrt(k0**2 * prism_permittivity - kx**2 + 0j)
     kz2 = np.sqrt(k0**2 - kx**2 + 0j)
@@ -110,17 +110,20 @@ def kvectors(wavenumber, E_xx, E_zz, E_xz, prism_permittivity = 5.5, angle=np.pi
         if kz3[i].imag<0:
             kz3[i] = kz3_other[i]
 
+    return k0, kx, kz1, kz2, kz3
 
-    return kx, kz1, kz2, kz3
 
+def plot_kvectors(wavenumber, kz3, kz3_other):
+    plt.rcParams["figure.figsize"] = (4,4)
+    fig, axs = plt.subplots(2)
+    fig.suptitle('k_vectors')
+    axs[0].plot(wavenumber, kz3.real)
+    axs[0].plot(wavenumber, kz3_other.real)
+    axs[0].set(xlabel='Wavenumber', ylabel = 'Real')
 
-def plot_kvectors(wavenumber, kz3):
-    plt.rcParams["figure.figsize"] = (5,4)
-    plt.suptitle('KVectors')
-    plt.plot(wavenumber, kz3.real)
-    plt.plot(wavenumber, kz3.imag)
-    plt.xlabel('Wavenumber')
-    plt.ylabel('kz3')
+    axs[1].plot(wavenumber, kz3.imag)
+    axs[1].plot(wavenumber, kz3_other.imag)
+    axs[1].set(xlabel='Wavenumber', ylabel = 'Imag')
 
     plt.show()
 
@@ -168,13 +171,13 @@ def fetch_reflectivity(kx,kz1,kz2,kz3, E_xx, E_zz, E_xz):
     return np.array(reflectivity)
 
 
-def plot_reflectivity(reflectivity):
-    plt.rcParams["figure.figsize"] = (2,4)
+def plot_reflectivity(wavenumber, reflectivity):
+    plt.rcParams["figure.figsize"] = (3,5)
     plt.plot(reflectivity,wavenumber)
     plt.xlabel('Reflectivity')
     plt.ylabel('$\omega/2\pi c (cm^{-1})$')
-    #plt.xlim(0,1)
-    plt.title('ATR for $\epsilon_{ext}$ = $\epsilon_{xx}$ P-Polarised')
+    plt.xlim(0,1)
+    plt.title('$d = 1.5 \mu m$, Rotation = 0 Degrees')
 
     plt.hlines(450,0,1,'black','dashed')
     plt.text(1.3, 450, '$\omega_{T2,ord}$', ha='right', va='center')
@@ -190,21 +193,107 @@ def plot_reflectivity(reflectivity):
 
     plt.show()
 
-
-
-
-if __name__ == "__main__":
+def main_one_scenario():
     params = permittivity_parameters()
     wavenumber = wavenumber_spectrum()
     E_ext, E_ord = permittivity_fetch(wavenumber, params)
     #plot_base_permittivity(wavenumber, E_ext, E_ord)
 
-    E_xx, E_zz, E_xz = rotated_permittivities(E_ext, E_ord)
+    E_xx, E_zz, E_xz = rotated_permittivities(E_ext, E_ord, rotation = 0.)
     #plot_rotated_permittivities(wavenumber, E_xx, E_zz, E_xz)
 
-    kx, kz1,kz2, kz3 = kvectors(wavenumber, E_xx, E_zz, E_xz)
+    k0, kx, kz1, kz2, kz3 = kvectors(wavenumber, E_xx, E_zz, E_xz)
 
-    # plot_kvectors(wavenumber, kz3)
+    #plot_kvectors(wavenumber, kz3, kz3_other)
 
     reflectivity = fetch_reflectivity(kx,kz1,kz2,kz3, E_xx, E_zz, E_xz)
-    plot_reflectivity(reflectivity)
+    plot_reflectivity(wavenumber, reflectivity)
+
+    
+def contour_single_rotation(wavenumber, x_axis, reflectivity, rotation = np.pi/4.):
+    
+    rotation = np.degrees(rotation)
+
+    x_axis = np.sqrt(5.5) * np.sin(x_axis)
+    params = {'mathtext.default': 'regular' }          
+    plt.rcParams.update(params)
+    plt.figure()
+    plt.pcolormesh(x_axis, wavenumber, reflectivity, cmap = 'magma')
+    cbar = plt.colorbar()
+    cbar.set_label("Reflectivity")
+    plt.title("ATR for Crystal Rotation of " + str({int(round(rotation,2))}) + " degrees, $d = 1.5 \mu m$")
+    plt.xlabel('$k_x / k_0 $')
+    plt.ylabel('$\omega/2\pi c (cm^{-1})$')
+    plt.savefig(f"plots/ATR_{int(rotation)}_degrees.png")
+    plt.close()
+
+
+
+def main_heatmap_wavenumber_rotation_reflectivity():
+    params = permittivity_parameters()
+    wavenumber = wavenumber_spectrum()
+    E_ext, E_ord = permittivity_fetch(wavenumber,params)
+    
+    reflectivities = []
+    angles = np.linspace(0,np.pi/2.,300)
+    for rotation_angle in angles:
+        E_xx, E_zz, E_xz = rotated_permittivities(E_ext, E_ord, rotation = rotation_angle)
+        kx,kz1,kz2,kz3 = kvectors(wavenumber, E_xx, E_zz, E_xz)
+        reflectivity = fetch_reflectivity(kx, kz1, kz2, kz3, E_xx, E_zz, E_xz)
+        reflectivities.append(reflectivity)
+    
+    reflectivities = np.array(reflectivities).reshape(len(wavenumber),len(reflectivities))
+
+
+def main_heatmap_single_rotation():
+    rotation_angle = 0.
+    params = permittivity_parameters()
+    wavenumber = wavenumber_spectrum()
+    E_ext, E_ord = permittivity_fetch(wavenumber,params)
+    
+    reflectivities = []
+    kx_list = []
+    k0_list = []
+    angles = np.linspace(-np.pi/2.,np.pi/2.,300)
+    for incident_angle in angles:
+        E_xx, E_zz, E_xz = rotated_permittivities(E_ext, E_ord, rotation = rotation_angle)
+        k0, kx,kz1,kz2,kz3 = kvectors(wavenumber, E_xx, E_zz, E_xz, angle = incident_angle)
+        reflectivity = fetch_reflectivity(kx, kz1, kz2, kz3, E_xx, E_zz, E_xz)
+        k0_list.append(k0)
+        kx_list.append(kx)
+        reflectivities.append(reflectivity)
+    k0_list=np.asarray(k0_list)
+    kx_list = np.asarray(kx_list)
+    reflectivities = np.array(reflectivities).reshape(len(wavenumber),len(reflectivities))
+
+    contour_single_rotation(wavenumber,angles, reflectivities.T)
+
+def main_heatmap_all_rotations():
+    rotation_angles = np.linspace(0, np.pi/2., 90)
+
+    params = permittivity_parameters()
+    wavenumber = wavenumber_spectrum()
+    E_ext, E_ord = permittivity_fetch(wavenumber,params)
+    angles = np.linspace(-np.pi/2.,np.pi/2.,300)
+    
+    for rotation_angle in rotation_angles:
+        reflectivities = []
+        kx_list = []
+        k0_list = []
+        for incident_angle in angles:
+            E_xx, E_zz, E_xz = rotated_permittivities(E_ext, E_ord, rotation = rotation_angle)
+            k0, kx,kz1,kz2,kz3 = kvectors(wavenumber, E_xx, E_zz, E_xz, angle = incident_angle)
+            reflectivity = fetch_reflectivity(kx, kz1, kz2, kz3, E_xx, E_zz, E_xz)
+            k0_list.append(k0)
+            kx_list.append(kx)
+            reflectivities.append(reflectivity)
+        k0_list=np.asarray(k0_list)
+        kx_list = np.asarray(kx_list)
+        reflectivities = np.array(reflectivities).reshape(len(wavenumber),len(reflectivities))
+
+        contour_single_rotation(wavenumber,angles, reflectivities.T, rotation_angle)
+
+
+
+if __name__ == "__main__":
+    main_one_scenario()
