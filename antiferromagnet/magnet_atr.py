9028fcae07878028a4906b4e7d20f65dd9684e99
Mark Cunningham <mcunningham0410@gmail.com>
2023-01-27 15:18:52 +0000

    permeabilities correct now with dumelow equations

--- antiferromagnet/magnet_atr.py
+++ antiferromagnet/magnet_atr.py
@@ -1,50 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sympy as sp
-
+from scipy import constants
 
 
 def magnetic_parameters():
-    gamma = 0.105 # cm^-1 / kOe
-    H0 = 3 # kOe
-    Ha = 197 # kOe
-    He = 533 # kOe
-    magnetisation = 560 # G
+    gamma = 1.05 # /cm /T
+    H0 = 0.3 # T 
+    Ha = 19.745 # T
+    He = 53.313 # T
+    magnetisation = 44563. # A/M
 
-    return gamma, H0, Ha, He, magnetisation
+    resonant_frequency_squared = gamma**2 * (2. * Ha * He + Ha**2.)
+    damping_parameter = 1.27e-4
 
-
-def calculate_permeability(frequency, gamma, H0, Ha, He, magnetisation):
-
-    resonant_frequency = np.sqrt(Ha * gamma**2 * (Ha + 2 * He))
-    damping_parameter = np.linspace(1.27e-4, 1.27e-6, len(frequency))
-    tau = 1/resonant_frequency * 1/damping_parameter
-
-    X = 1/ ((frequency + H0 * gamma + 1j/tau)**2 - resonant_frequency**2)
-    Y = 1/ ((frequency - H0 * gamma + 1j/tau)**2 - resonant_frequency**2)
-
-    mu_3 = 1 - 4 * np.pi * gamma**2 * magnetisation * Ha * (X+Y)
-    mu_t = 4 * np.pi * gamma**2 * magnetisation * Ha * (Y-X)
-
-    return mu_3, mu_t
+    return gamma, H0, Ha, magnetisation, resonant_frequency_squared, damping_parameter
 
 
-def plot_permeability_components(frequency, mu_3, mu_t):
-    plt.rcParams["figure.figsize"] = (8,7)
-    fig, axs = plt.subplots(2)
-    fig.suptitle('Permeabilities')
-    axs[0].plot(frequency, mu_3.real, label = 'Real')
-    axs[0].plot(frequency, mu_3.imag, label = 'Imaginary')
-    axs[0].set(xlabel='$\omega/2\pi c (cm^{-1})$', ylabel = '$\mu_{3}$')
-    axs[0].legend()
+def calculate_permeability(frequency, gamma, H0, Ha, magnetisation, resonant_frequency_squared, damping):
 
-    axs[1].plot(frequency, mu_t.real, label = 'Real')
-    axs[1].plot(frequency, mu_t.imag, label = 'Imaginary')
-    axs[1].set(xlabel='$\omega/2\pi c (cm^{-1})$', ylabel = '$\mu_{t}$')
-    axs[1].legend()
+    X = 1./ (resonant_frequency_squared - (frequency + gamma * H0 + 1.j * damping)**2)
+    Y = 1./ (resonant_frequency_squared - (frequency - gamma * H0 + 1.j * damping)**2)
+    
+    mu_3 = 1. + (constants.mu_0 * gamma**2 * Ha * magnetisation * (X+Y))
+    mu_t = constants.mu_0 * gamma**2 * Ha * magnetisation * (X-Y)
 
-    plt.show()
-    plt.close()
+    mu_v = (mu_3**2. - mu_t**2.)/mu_3
+
+    return mu_3, mu_t, mu_v
 
 
 def retrieve_matrix(frequency, permeability_3, permeability_t):
@@ -58,14 +41,14 @@ def retrieve_matrix(frequency, permeability_3, permeability_t):
     element_6 = ky * kz
     element_7 = kx * kz
     element_8 = ky * kz
-    element_9 = 1. # (k0**2 * epsilon - kx**2 - ky**2)
+    element_9 = (k0**2 * epsilon - kx**2 - ky**2)
 
     matrix = sp.Matrix([element_1, element_2, element_3, element_4, element_5, element_6, element_7, element_8, element_9]).reshape(3,3)
     
     new_matrices = [matrix.subs({
         k0 : 2 * np.pi * omega,
         kx : np.sin(np.pi/4.) * 2 * np.pi * omega * np.sqrt(11.56),
-        kz : 0 + 0j,
+        kz : 0. + 0j,
         epsilon : 5.5 + 0j,
         mu_3 : item_1 + 0j,
         mu_t : item_2 + 0j
@@ -77,10 +60,8 @@ def retrieve_matrix(frequency, permeability_3, permeability_t):
 def solve_ky(matrices):
     determinants = [item.det() for item in matrices]
     ky_solutions = [sp.solve(item, sp.symbols('k_y')) for item in determinants]
-
-    ky_solutions = np.asarray(ky_solutions).astype(complex)
     
-    return ky_solutions
+    return np.asarray(ky_solutions).astype(complex)
 
 
 def plot_ky(frequency, ky_solutions):
@@ -89,7 +70,6 @@ def plot_ky(frequency, ky_solutions):
     first_solutions = ky_solutions[:,1]
     second_solutions = ky_solutions[:,2]
     third_solutions = ky_solutions[:,3]
-    
     
     plt.rcParams["figure.figsize"] = (8,7)
     fig, axs = plt.subplots(4)
@@ -118,6 +98,8 @@ def plot_ky(frequency, ky_solutions):
     plt.close()
 
 
+
+
 def plot_analytical_and_matrix(frequency, analytical_ky, analytical_ky_other, matrix_ky, matrix_ky_other):
     plt.rcParams["figure.figsize"] = (8,7)
     fig, axs = plt.subplots(2)
@@ -138,33 +120,105 @@ def plot_analytical_and_matrix(frequency, analytical_ky, analytical_ky_other, ma
 
     plt.show()
 
+def plot_permeability_ky(frequency, mu_3, mu_t, ky):
+    plt.rcParams["figure.figsize"] = (8,7)
+    fig, axs = plt.subplots(3)
+    fig.suptitle('Permeabilities and ky')
+    axs[0].plot(frequency, mu_3.real, label = 'Real')
+    axs[0].plot(frequency, mu_3.imag, label = 'Imaginary')
+    axs[0].set(xlabel='$\omega/2\pi c (cm^{-1})$', ylabel = '$\mu_{3}$')
+    axs[0].legend()
 
-def no_phi(wavenumber, permeability_3, permeability_t, magnet_permittivity=5.5, prism_permittivity=11.56, incident_angle=np.pi/4.):
-
-    k0 = wavenumber * 2 * np.pi
-    kx = k0 * np.sqrt(prism_permittivity) * np.sin(incident_angle)
+    axs[1].plot(frequency, mu_t.real, label = 'Real')
+    axs[1].plot(frequency, mu_t.imag, label = 'Imaginary')
+    axs[1].set(xlabel='$\omega/2\pi c (cm^{-1})$', ylabel = '$\mu_{t}$')
+    axs[1].legend()
     
-    ky = 0. - np.sqrt((k0**2 * magnet_permittivity * (permeability_3**2 - permeability_t**2)/permeability_3) - kx**2)
+    axs[2].plot(frequency, ky.real, label = 'Real')
+    axs[2].plot(frequency, ky.imag, label = 'Imaginary')
+    axs[2].set(xlabel='$\omega/2\pi c (cm^{-1})$', ylabel = '$k_{y}$')
+    axs[2].legend()
 
-    ky_other = 0. + np.sqrt((k0**2 * magnet_permittivity * (permeability_3**2 - permeability_t**2)/permeability_3) - kx**2)
+    plt.show()
+    plt.close()
 
-    return ky, ky_other
 
+def plot_permeability_components(frequency, permeability_3, permeability_t, mu_v):
+
+    plt.rcParams["figure.figsize"] = (8,7)
+    fig, axs = plt.subplots(3)
+    fig.suptitle('Permeabilities and $\mu_{v}$')
+    axs[0].plot(frequency, permeability_3.real, label = 'Real')
+    axs[0].plot(frequency, permeability_3.imag, label = 'Imaginary')
+    axs[0].set(xlabel='$\omega/2\pi c (cm^{-1})$', ylabel = '$\mu_{3}$')
+    axs[0].legend()
+
+    axs[1].plot(frequency, permeability_t.real, label = 'Real')
+    axs[1].plot(frequency, permeability_t.imag, label = 'Imaginary')
+    axs[1].set(xlabel='$\omega/2\pi c (cm^{-1})$', ylabel = '$\mu_{t}$')
+    axs[1].legend()
+    
+    axs[2].plot(frequency, mu_v.real, label = 'Real')
+    axs[2].plot(frequency, mu_v.imag, label = 'Imaginary')
+    axs[2].set(xlabel='$\omega/2\pi c (cm^{-1})$', ylabel = '$\mu_{v}$')
+    axs[2].legend()
+
+    plt.show()
+    plt.close()
+
+
+def fetch_reflectivity(frequency, mu_3, mu_t, mu_v,  incidence = 0., permittivity = 5.5):
+
+    k0 = frequency * 2. * np.pi + 0.j
+    kx = k0 * np.sin(incidence) * np.sqrt(11.56) + 0.j
+    
+    k1y = np.sqrt(k0**2 * 11.56 - kx**2 + 0.j)
+
+    k2y = np.sqrt(permittivity * mu_v * k0**2 - kx**2)
+
+    top_line = k1y * mu_v - k2y - 1.j * kx * (mu_t/mu_3)
+    bottom_line = k1y * mu_v + k2y + 1.j * kx * (mu_t/mu_3)
+
+    reflectivity = top_line/bottom_line
+
+    return reflectivity * np.conjugate(reflectivity)
+
+
+def plot_reflectivity(wavenumber, reflectivity, incident_angle):
+    plt.rcParams["figure.figsize"] = (3,5)
+    params = {'mathtext.default': 'regular' }          
+    plt.rcParams.update(params)
+    plt.plot(reflectivity, wavenumber)
+    plt.xlabel('Reflectivity')
+    plt.ylabel('$\omega/2\pi c (cm^{-1})$')
+    #plt.xlim(0,1)
+    plt.title(r'ATR for $\theta$ = ${}^\circ$ S-Polarised'.format(np.degrees(incident_angle)))
+
+    plt.show()
+    plt.close()
+    
 
 
 def main():
-    frequency = np.linspace(52.01, 54, 250)
+    frequency = np.linspace(52.01, 54, 1000)
     parameters = magnetic_parameters()
-    permeability_3, permeability_t = calculate_permeability(*(frequency, *parameters))
+    permeability_3, permeability_t, mu_v = calculate_permeability(*(frequency, *parameters))
+    
+    #plot_permeability_components(frequency, permeability_3, permeability_t, mu_v)
 
-    matrices = retrieve_matrix(frequency, permeability_3, permeability_t)
-    ky_solutions = solve_ky(matrices)
-    ky_matrix = ky_solutions[:,0]
-    ky_matrix_other = ky_solutions[:,1]
+    #matrices = retrieve_matrix(frequency, permeability_3, permeability_t)
+    #ky_solutions = solve_ky(matrices)
+    #ky_matrix = ky_solutions[:,3]
+    #ky_matrix_other = ky_solutions[:,0]
 
-    ky_analytical, ky_other_analytical = no_phi(frequency, permeability_3, permeability_t)
+    #plot_permeability_ky(frequency, permeability_3, permeability_t, ky_matrix)
+    #plot_permeability_ky(frequency, permeability_3, permeability_t, ky_other_analytical)
 
-    plot_analytical_and_matrix(frequency, ky_analytical, ky_other_analytical, ky_matrix, ky_matrix_other)
+    reflectivity = fetch_reflectivity(frequency, permeability_3, permeability_t, mu_v)
+    plot_reflectivity(frequency, reflectivity, 0.)
+
+
+    # plot_analytical_and_matrix(frequency, ky_analytical, ky_other_analytical, ky_matrix, ky_matrix_other)
 
 
 if __name__ == "__main__":
