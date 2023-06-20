89a92bb7835da8443723e24c6adc5d58ddcecc0c
Mark Cunningham <mcunningham0410@gmail.com>
2023-06-20 16:02:05 +0100

    linted script.py

--- script.py
+++ script.py
@@ -1,3 +1,10 @@
+"""
+script.py
+
+Takes JSON payload containing data on multilayer structure,
+creates the structure object then produces the reflectivity spectra.
+"""
+
 import json
 from structure import Structure
 from payloads import mock_incident_payload, mock_azimuthal_payload, mock_dispersion_payload
@@ -7,7 +14,15 @@ def main():
     """
     Main function
     """
-    payload = json.loads(mock_dispersion_payload())
+    mode = 'incident'
+    if mode == 'incident':
+        payload = json.loads(mock_incident_payload())
+    elif mode == 'azimuthal':
+        payload = json.loads(mock_azimuthal_payload())
+    elif mode == 'dispersion':
+        payload = json.loads(mock_dispersion_payload())
+    else:
+        raise NotImplementedError(f"Mode {mode} not implemented")
     structure = Structure()
     structure.execute(payload)
     structure.plot()
