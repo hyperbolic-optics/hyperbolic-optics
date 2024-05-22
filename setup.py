d54eb1add6ac3d6709a9b6e2702c215a3b29b695
Mark Cunningham <42680547+MarkCunningham0410@users.noreply.github.com>
2024-05-22 18:04:29 +0100

    Update setup.py
    fixing name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='hyperbolic-optics',
+    name='hyperbolic_optics',
     version='1.0.0',
     packages=find_packages(),
     install_requires=[
